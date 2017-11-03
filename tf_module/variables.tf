00f925242c9233e896daddeed8b7e04c88f9bc1b
martinbjorgan <martin.bjorgan@evry.com>
2017-11-03 14:33:47 +0100

    Create variables.tf

---
+++ tf_module/variables.tf
@@ -0,0 +1,9 @@
+variable modulename {}
+variable SubnetId {
+  default = "subnet-00767c69"
+  description = "The subnet id of a default vpc in the working system"
+}
+
+variable SecurityGroupId {
+  description = "this variable doesn't have a default value but it is passed from the main code"
+}

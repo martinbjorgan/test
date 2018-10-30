254318ef9cb967347879ec5631c419d8d7702580
martinbjorgan <martin.bjorgan@evry.com>
2018-10-30 12:11:44 +0100

    Update jau.ps1

--- jau.ps1
+++ jau.ps1
@@ -1,4 +1,4 @@
 $yamlpath = "C:\ProgramData\Datadog\datadog.yaml"
-$dlpath = "C:/Temp/datadog-agent-6-latest.amd64.msi"
+$dlpath = "C:/datadog-agent-6-latest.amd64.msi"
 Remove-Item –path $yamlpath
-(New-Object System.Net.WebClient).DownloadFile("https://s3.amazonaws.com/ddagent-windows-stable/datadog-agent-6-latest.amd64.msi", $dlpath) 
+(New-Object System.Net.WebClient).DownloadFile("https://s3.amazonaws.com/ddagent-windows-stable/datadog-agent-6-latest.amd64.msi", $dlpath)
